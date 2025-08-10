3a5e7b427fa429167b0012efa19a22f827299963
rohitdeka-1 <studyrhd@gmail.com>
2025-08-10 22:28:03 +0530

    react page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="resume-embed-full">
+      <iframe
+        title="Rohit Deka Resume"
+        src="https://drive.google.com/file/d/1BiZDhPtSvgM7OUxEwfgQ_nga1gJmA2O7/preview"
+        allow="autoplay"
+        frameBorder="0"
+        className="resume-iframe"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
