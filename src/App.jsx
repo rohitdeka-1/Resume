bc4d04bb209a8c92fc1a220f44b593ba6f3ba417
rohitdeka-1 <studyrhd@gmail.com>
2025-08-13 14:07:06 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="resume-embed-full">
       <iframe
         title="Rohit Deka Resume"
-        src="https://drive.google.com/file/d/12B9Erc_kRFlNDBQOgM4eW8cB6sGYqRGf/view"
+        src="https://drive.google.com/file/d/12B9Erc_kRFlNDBQOgM4eW8cB6sGYqRGf/preview"
         allow="autoplay"
         frameBorder="0"
         className="resume-iframe"
